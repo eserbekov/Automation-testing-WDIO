5c2b6acecacd2d4ba13f9ffbd584b469c3e37d85
Murat Eserbekov <murat_eserbekov@mail.ru>
2020-02-02 23:32:57 +0200

    Install chai.

--- test/specs/register.js
+++ test/specs/register.js
@@ -1,11 +1,21 @@
-const assert = require('assert')
+const {expect} = require('chai');
 
 describe('Register page', () => {
-    it('should have the right title', () => {
+    before(()=> {
         browser.url('https://stage.pasv.us/user/register')
-        const title = browser.getTitle()
-        assert.strictEqual(title, 'Progress Monitor')
-    })
+    });
+
+    it('should have the right title', () => {
+        const actualTitle = browser.getTitle();
+        const expectedTitle = 'Progress Monitor';
+        expect(actualTitle).equal(expectedTitle);
+    });
+
+    it('should have a correct title', ()=>{
+        const actualH1Text = $('h1').getText();
+        const expectedH1Text = 'User Register';
+        expect(actualH1Text).equal(expectedH1Text);
+    });
 
 
 
